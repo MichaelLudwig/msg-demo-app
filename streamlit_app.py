a20af6e997fde9c09f90fb715b1f2dc474f10096
MichaelLudwig <michael.ludwig3@gmail.com>
2024-09-02 17:31:00 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,8 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-from io import BytesIO
-from docx import Document
-from docx.shared import Inches
-from PIL import Image
+
 
 st.page_link("streamlit_app.py", label="Home", icon="🏠")
-st.page_link("pages/charts.py", label="Architecture Demo", icon="1️⃣")
+#st.page_link("pages/charts.py", label="Architecture Demo", icon="1️⃣")
 st.page_link("pages/charts.py", label="Chart Demo", icon="2️⃣")
 st.page_link("pages/charts.py", label="Chart Demo", icon="3️⃣")
 
