a9385c13c9d64187572e46a9791d39afaeba4f1c
MichaelLudwig <michael.ludwig3@gmail.com>
2024-09-02 17:55:15 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,5 +16,21 @@ st.graphviz_chart(dot)
 
 
 st.page_link("pages/charts_demo.py", label="Chart Demo", icon="📊")
+def get_chart_98552(use_container_width: bool):
+
+    source = data.stocks()
+
+    chart = alt.Chart(source).mark_line(point=True).encode(
+        x='date:T',
+        y='price:Q',
+        color='symbol:N'
+    )
+
+    st.altair_chart(chart, theme="streamlit", use_container_width=True)
+
+
+get_chart_98552(True)
+
+
 st.page_link("pages/report_demo.py", label="Report Demo", icon="📝")
 
