946b1640a1761f4ac6ee3acfa79dab5be0f6cd92
MichaelLudwig <michael.ludwig3@gmail.com>
2024-09-02 16:08:09 +0200

    .

--- pages/architecture_demo.py
+++ pages/architecture_demo.py
@@ -1,30 +1,18 @@
 import streamlit as st
-import graphviz
-
-st.title("AWS Architektur")
-
-# Ein einfaches Diagramm erstellen
-dot = """
-digraph G {
-    EC2 [label="EC2 Instance", shape=box]
-    S3 [label="S3 Bucket", shape=cylinder]
-    EC2 -> S3
-}
-"""
-
-st.graphviz_chart(dot)
-
-
-
-from diagrams import Diagram, Cluster
+from diagrams import Diagram
 from diagrams.aws.compute import EC2
-from diagrams.aws.storage import S3
+from diagrams.aws.database import RDS
+from diagrams.aws.network import ELB
 
-# Diagramm erstellen und als Bild speichern
-with Diagram("AWS Architektur", show=False):
-    ec2 = EC2("EC2 Instance")
-    s3 = S3("S3 Bucket")
-    ec2 >> s3
+st.title("AWS Architekturdiagramm")
+
+# Diagramm erstellen
+with Diagram("Simple AWS Web Service", show=False, outformat="png", filename="aws_architecture"):
+    elb = ELB("Load Balancer")
+    ec2 = EC2("Web Server")
+    rds = RDS("Database")
+
+    elb >> ec2 >> rds
 
 # Bild in Streamlit anzeigen
-st.image("aws_architektur_diagram.png", caption="Meine AWS Architektur")
+st.image("aws_architecture.png", caption="Einfaches AWS Web Service Diagramm")
