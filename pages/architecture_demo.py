15396b34b3afeef77dccfb343bd053ec13075d54
MichaelLudwig <michael.ludwig3@gmail.com>
2024-09-02 16:09:04 +0200

    .

--- pages/architecture_demo.py
+++ pages/architecture_demo.py
@@ -1,18 +1,27 @@
-import streamlit as st
-from diagrams import Diagram
+from diagrams import Diagram, Cluster
 from diagrams.aws.compute import EC2
+from diagrams.aws.network import ELB, Route53
 from diagrams.aws.database import RDS
-from diagrams.aws.network import ELB
+from diagrams.aws.storage import S3
 
-st.title("AWS Architekturdiagramm")
+st.title("Komplexere AWS Architektur")
 
 # Diagramm erstellen
-with Diagram("Simple AWS Web Service", show=False, outformat="png", filename="aws_architecture"):
-    elb = ELB("Load Balancer")
-    ec2 = EC2("Web Server")
-    rds = RDS("Database")
+with Diagram("Web Service Architecture", show=False, outformat="png", filename="complex_aws_architecture"):
+    dns = Route53("DNS")
+    lb = ELB("Load Balancer")
 
-    elb >> ec2 >> rds
+    with Cluster("Web Servers"):
+        web_servers = [EC2("web1"),
+                       EC2("web2"),
+                       EC2("web3")]
+
+    db = RDS("User Database")
+    storage = S3("User Data")
+
+    dns >> lb >> web_servers
+    web_servers >> db
+    web_servers >> storage
 
 # Bild in Streamlit anzeigen
-st.image("aws_architecture.png", caption="Einfaches AWS Web Service Diagramm")
+st.image("complex_aws_architecture.png", caption="Komplexeres AWS Architekturdiagramm")
