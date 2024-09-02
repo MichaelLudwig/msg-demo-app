893da4b313c5796ba0780c63654e25886f95e2ae
MichaelLudwig <michael.ludwig3@gmail.com>
2024-09-02 17:30:26 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
-
+import matplotlib.pyplot as plt
+from io import BytesIO
+from docx import Document
+from docx.shared import Inches
+from PIL import Image
 
 st.page_link("streamlit_app.py", label="Home", icon="🏠")
 st.page_link("pages/charts.py", label="Architecture Demo", icon="1️⃣")
