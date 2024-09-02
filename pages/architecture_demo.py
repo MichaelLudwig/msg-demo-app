44b3bb36192c2c7450f3d8440f8c179c45bb76b1
MichaelLudwig <michael.ludwig3@gmail.com>
2024-09-02 16:19:45 +0200

    .

--- pages/architecture_demo.py
+++ pages/architecture_demo.py
@@ -1,6 +1,19 @@
 import streamlit as st
 import graphviz
 
+st.title("AWS Architektur")
+
+# Ein einfaches Diagramm erstellen
+dot = """
+digraph G {
+    EC2 [label="EC2 Instance", shape=box]
+    S3 [label="S3 Bucket", shape=cylinder]
+    EC2 -> S3
+}
+"""
+
+st.graphviz_chart(dot)
+
 st.graphviz_chart('''
     digraph {
         run -> intr
