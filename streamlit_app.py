5073a06d334c656445ecf298b750a700267804c9
MichaelLudwig <michael.ludwig3@gmail.com>
2024-09-02 17:35:58 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ import streamlit as st
 st.page_link("streamlit_app.py", label="Home", icon="🏠")
 st.page_link("pages/architecture_demo.py", label="Architecture Demo", icon="1️⃣")
 st.page_link("pages/charts_demo.py", label="Chart Demo", icon="2️⃣")
-st.page_link("pages/report_demo.py", label="Chart Demo", icon="3️⃣")
+st.page_link("pages/report_demo.py", label="Report Demo", icon="📝")
 
