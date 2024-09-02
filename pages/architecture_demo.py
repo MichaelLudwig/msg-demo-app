12351da71c24b32a0375bc08dba98c11eb27b3c1
MichaelLudwig <michael.ludwig3@gmail.com>
2024-09-02 17:12:52 +0200

    .

--- pages/architecture_demo.py
+++ pages/architecture_demo.py
@@ -74,38 +74,3 @@ digraph G {
 st.graphviz_chart(dot)
 
 
-from diagrams import Cluster, Diagram
-from diagrams.aws.compute import ECS
-from diagrams.aws.database import ElastiCache, RDS
-from diagrams.aws.network import ELB
-from diagrams.aws.network import Route53
-
-with Diagram("Clustered Web Services", show=False):
-    dns = Route53("dns")
-    lb = ELB("lb")
-
-    with Cluster("Services"):
-        svc_group = [ECS("web1"),
-                     ECS("web2"),
-                     ECS("web3")]
-
-    with Cluster("DB Cluster"):
-        db_primary = RDS("userdb")
-        db_primary - [RDS("userdb ro")]
-
-    memcached = ElastiCache("memcached")
-
-    dns >> lb >> svc_group
-    svc_group >> db_primary
-    svc_group >> memcached
-
-
-with Diagram("Simple AWS Web Service", show=False, outformat="png", filename="aws_architecture"):
-    elb = ELB("Load Balancer")
-    ec2 = EC2("Web Server")
-    rds = RDS("Database")
-
-    elb >> ec2 >> rds
-
-# Bild in Streamlit anzeigen
-st.image("aws_architecture.png", caption="Einfaches AWS Web Service Diagramm")
