ba7a1d8a78aa879f6671a26a0de6f7156ce67efa
MichaelLudwig <michael.ludwig3@gmail.com>
2024-09-04 10:51:24 +0200

    .

--- pages/report_demo.py
+++ pages/report_demo.py
@@ -187,7 +187,7 @@ if st.button('Speichern'):
     row_cells = table.add_row().cells
     row_cells[0].text = "Gesamteinschätzung"
     row_cells[1].text = gesamt_einschaetzung
-    row_cells[2].text = "Die getroffenen Maßnahmen im Brandschutz des betrachteten 10-stöckigen Gebäudes mit 100 Wohneinheiten erfüllen die grundlegenden Anforderungen weitgehend. Insbesondere der Einsatz von Brandschutztüren und die Sicherstellung der Löschwasserversorgung sind sehr gut umgesetzt. Verbesserungsbedarf gibt es bei der Installation der Brandmeldeanlagen und der Sicherheitsbeleuchtung, wo nur minimale Anforderungen erfüllt wurden. Insgesamt wird der Fertigstellungsgrad mit einem soliden Durchschnitt von etwa 75 % bewertet, was einer "guten" Einstufung entspricht. Um den optimalen Schutz zu gewährleisten, sollten insbesondere die Maßnahmen in Fluchtwegen und bei der Überwachung von Notrufsystemen verbessert werden."
+    row_cells[2].text = "Die getroffenen Maßnahmen im Brandschutz des betrachteten 10-stöckigen Gebäudes mit 100 Wohneinheiten erfüllen die grundlegenden Anforderungen weitgehend. Insbesondere der Einsatz von Brandschutztüren und die Sicherstellung der Löschwasserversorgung sind sehr gut umgesetzt. Verbesserungsbedarf gibt es bei der Installation der Brandmeldeanlagen und der Sicherheitsbeleuchtung, wo nur minimale Anforderungen erfüllt wurden. Insgesamt wird der Fertigstellungsgrad mit einem soliden Durchschnitt von etwa 75 Prozent bewertet, was einer 'guten' Einstufung entspricht. Um den optimalen Schutz zu gewährleisten, sollten insbesondere die Maßnahmen in Fluchtwegen und bei der Überwachung von Notrufsystemen verbessert werden."
 
     # Bild für den Gesamtfertigstellungsgrad hinzufügen
     average_donut_chart = create_donut_chart(average_percentage)
