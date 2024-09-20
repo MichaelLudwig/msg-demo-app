6fc2f7e51bf163f72877ca5c8d841502bdde62c8
MichaelLudwig <michael.ludwig3@gmail.com>
2024-09-20 13:24:23 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,18 +2,23 @@ import streamlit as st
 
 
 st.page_link("streamlit_app.py", label="Home", icon="🏠")
+
 st.divider()
 
 st.page_link("pages/architecture_demo.py", label="Architecture Demo", icon="☁️")
 st.image("architecture.png", width=300)
+
 st.divider()
 
 st.page_link("pages/charts_demo.py", label="Chart Demo", icon="📊")
 st.image("chart.png", width=300)
+
 st.divider()
 
 st.page_link("pages/report_demo.py", label="Report Demo", icon="📝")
 st.image("report.png", width=300)
+
+st.divider()
 
 st.page_link("pages/text-to-chart.py", label="AI Text-Chart Demo", icon="📊")
 st.image("gantt.png", width=300)
