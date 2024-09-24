e0721f05b591b61c16a0b4fc653198b716b2cec8
MichaelLudwig <michael.ludwig3@gmail.com>
2024-09-24 17:26:28 +0200

    .

--- pages/azure-openai-api-test.py
+++ pages/azure-openai-api-test.py
@@ -27,7 +27,7 @@ if prompt := st.chat_input("Hi"):
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
-        for response in client.ChatCompletion.create(
+        for response in client.chat.completions.create(
             model=openAI_model,
             messages=[{"role": m["role"], "content": m["content"]}
                       for m in st.session_state.messages], stream=True):
