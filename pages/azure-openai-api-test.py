ca5d44914309ecf452f8cc3ff4fc0f2568ddf19e
MichaelLudwig <michael.ludwig3@gmail.com>
2024-09-24 17:44:40 +0200

    .

--- pages/azure-openai-api-test.py
+++ pages/azure-openai-api-test.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import openai
 
+import os
+import json
+
 # OpenAI API-Client initialisieren
 client = openai.AzureOpenAI(
     api_key="1d304241086e4f81adf346216e983c59",
@@ -10,29 +13,39 @@ client = openai.AzureOpenAI(
 )
 openAI_model = "gpt-4o-mini-sw"
 
-# Streamlit-Seite konfigurieren
-st.set_page_config(page_title="Chat-GPT mit Azure OpenAI Service", page_icon="🤖")
+# initialize chat session in streamlit if not already present
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
 
+# streamlit page title
+st.title("🤖 GPT-4o - ChatBot")
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-for message in st.session_state.messages:
+# display chat history
+for message in st.session_state.chat_history:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("Hi"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
+
+# input field for user's message
+user_prompt = st.chat_input("Ask GPT-4o...")
+
+if user_prompt:
+    # add user's message to chat and display it
+    st.chat_message("user").markdown(user_prompt)
+    st.session_state.chat_history.append({"role": "user", "content": user_prompt})
+
+    # send user's message to GPT-4o and get a response
+    response = openai.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant"},
+            *st.session_state.chat_history
+        ]
+    )
+
+    assistant_response = response.choices[0].message.content
+    st.session_state.chat_history.append({"role": "assistant", "content": assistant_response})
+
+    # display GPT-4o's response
     with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-        for response in client.chat.completions.create(
-            model=openAI_model,
-            messages=[{"role": m["role"], "content": m["content"]}
-                      for m in st.session_state.messages], stream=True):
-            full_response += response.choices[0].delta.get("content", "")
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+        st.markdown(assistant_response)
