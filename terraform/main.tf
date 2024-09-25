8db9dc5fd9e2307983d1cd133dadf39ce625536b
MichaelLudwig <michael.ludwig3@gmail.com>
2024-09-25 12:06:19 +0200

    .

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,16 @@
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "streamlit-demo" {
   name     = "streamlit-demo-resource-group"
   location = "Sweden Central"  # oder "Sweden North"
 }
 
-resource "azurerm_service_plan" "streamlit-demo" {
+resource "azurerm_app_service_plan" "streamlit-demo" {
   name                = "streamlit-demo-appservice-plan"
   location            = azurerm_resource_group.streamlit-demo.location
   resource_group_name = azurerm_resource_group.streamlit-demo.name
 
   os_type             = "Linux"
-  sku_name            = "Shared"      # Die Größe des Serviceplans
-  sku_tier            = "Sd1"    # Tier des Serviceplans
+  sku_name            = "B1"      # Die Größe des Serviceplans
+  sku_tier            = "Basic"    # Tier des Serviceplans
   sku_capacity        = 1           # Anzahl der Instanzen
 }
 
@@ -22,7 +18,7 @@ resource "azurerm_app_service" "streamlit-demo" {
   name                = "streamlit-demo-streamlit-app"
   location            = azurerm_resource_group.streamlit-demo.location
   resource_group_name = azurerm_resource_group.streamlit-demo.name
-  app_service_plan_id = azurerm_service_plan.streamlit-demo.id
+  app_service_plan_id = azurerm_app_service_plan.streamlit-demo.id
 
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = "1"
