b29bb55679e33e35877b3a70158cf39e16eac4d1
MichaelLudwig <michael.ludwig3@gmail.com>
2024-09-25 11:35:28 +0200

    .

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "streamlit-demo" {
   location = "Sweden Central"  # oder "Sweden North"
 }
 
-resource "azurerm_app_service_plan" "streamlit-demo" {
+resource "azurerm_service_plan" "streamlit-demo" {
   name                = "streamlit-demo-appservice-plan"
   location            = azurerm_resource_group.streamlit-demo.location
   resource_group_name = azurerm_resource_group.streamlit-demo.name
@@ -22,7 +22,7 @@ resource "azurerm_app_service" "streamlit-demo" {
   name                = "streamlit-demo-streamlit-app"
   location            = azurerm_resource_group.streamlit-demo.location
   resource_group_name = azurerm_resource_group.streamlit-demo.name
-  app_service_plan_id = azurerm_app_service_plan.streamlit-demo.id
+  app_service_plan_id = azurerm_service_plan.streamlit-demo.id
 
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = "1"
