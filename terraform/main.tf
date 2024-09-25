f47f41d597de37d44e78bcea38059427fed2c72c
MichaelLudwig <michael.ludwig3@gmail.com>
2024-09-25 11:28:35 +0200

    Terraform Skript und github Action zum Azure Ressource deployment

---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "streamlit-demo" {
+  name     = "streamlit-demo-resource-group"
+  location = "Sweden Central"  # oder "Sweden North"
+}
+
+resource "azurerm_app_service_plan" "streamlit-demo" {
+  name                = "streamlit-demo-appservice-plan"
+  location            = azurerm_resource_group.streamlit-demo.location
+  resource_group_name = azurerm_resource_group.streamlit-demo.name
+
+  sku {
+    tier = "Shared"  
+    size = "Sd1"    
+  }
+}
+
+resource "azurerm_app_service" "streamlit-demo" {
+  name                = "streamlit-demo-streamlit-app"
+  location            = azurerm_resource_group.streamlit-demo.location
+  resource_group_name = azurerm_resource_group.streamlit-demo.name
+  app_service_plan_id = azurerm_app_service_plan.streamlit-demo.id
+
+  app_settings = {
+    "WEBSITE_RUN_FROM_PACKAGE" = "1"
+  }
+}
