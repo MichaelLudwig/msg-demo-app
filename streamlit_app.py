e212714d69c1c5a59308031cec653e697324137d
MichaelLudwig <michael.ludwig3@gmail.com>
2024-10-02 13:47:37 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-st.set_page_config(page_title="Demo Apps", page_icon=":mobile_phone:", layout="wide")
+st.set_page_config(page_title="Demo Apps", page_icon=":iphone:", layout="wide")
 main_heading=st.title("Demo Apps")
 
 st.divider()
