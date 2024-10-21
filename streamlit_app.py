6d38cc3de2471f4af3872877771f9c4452a50db6
MichaelLudwig <michael.ludwig3@gmail.com>
2024-10-21 17:55:08 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ st.divider()
 st.page_link("pages/report_demo.py", label="Report Demo", icon="📝")
 st.image("report.png", width=300)
 
-st.divider()
+#st.divider()
 
 #st.page_link("pages/text-to-chart.py", label="AI Text-Chart Demo", icon="📊")
 #st.image("gantt.png", width=300)
